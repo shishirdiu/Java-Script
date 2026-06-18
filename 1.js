acdd817b8ad6c5d7e6515387b0991bfc493c2ae3
Shishir Sarker <shishirsarker78@gmail.com>
2026-06-18 11:49:07 +0600

    Add full name prompt and related console logs

--- 1.js
+++ 1.js
@@ -1,1 +1,8 @@
+let firstName = prompt("Enter Your First Name:");
+let lastName = prompt("Enter Your Last Name:");
 
+let fullName = firstName+ " "+ lastName;
+console.log( "Your Full Name:" + fullName);
+console.log("Length Of Your Full Name:" + fullName.length);
+console.log("UpperCase Of Your Full Name:"+  fullName.toUpperCase());
+console.log(" 2nd Character Of Your Full Name: " + fullName.charAt(1));
